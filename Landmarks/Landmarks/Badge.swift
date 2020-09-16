8a1a2d8d48f29006b4a0df33fb8e494dce8d2cff
technosoul <rockdrigo.mtz@gmail.com>
2020-09-16 16:22:11 -0500

    step 5 completed :boom:

--- Landmarks/Landmarks/Badge.swift
+++ Landmarks/Landmarks/Badge.swift
@@ -13,13 +13,28 @@ struct Badge: View {
         Path { path in
             let width: CGFloat = 100.0
             let height = width
-            path.move(to: CGPoint(x: width * 0.95, y: height * 0.20))
+            path.move(
+                to: CGPoint(
+                    x: width * 0.95,
+                    y: height * (0.20 + HexagonParameters.adjustment)
+                )
+            )
             
             HexagonParameters.points.forEach {
                 path.addLine(
                     to: .init(
                         x: width * $0.useWidth.0 * $0.xFactors.0,
                         y: height * $0.useHeight.0 * $0.yFactors.0
+                    )
+                )
+                path.addQuadCurve(
+                    to: .init(
+                        x: width * $0.useWidth.1 * $0.xFactors.1,
+                        y: height * $0.useHeight.1 * $0.yFactors.1
+                    ),
+                    control: .init(
+                        x: width * $0.useWidth.2 * $0.xFactors.2,
+                        y: height * $0.useHeight.2 * $0.yFactors.2
                     )
                 )
             }
