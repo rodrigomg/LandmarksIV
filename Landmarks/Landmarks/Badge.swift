3f04c5a58a650f052dd877fe6e935d8a54ff2287
technosoul <rockdrigo.mtz@gmail.com>
2020-09-16 16:19:19 -0500

    step 2,3 and 4 completed :boom:

--- Landmarks/Landmarks/Badge.swift
+++ Landmarks/Landmarks/Badge.swift
@@ -10,7 +10,21 @@ import SwiftUI
 
 struct Badge: View {
     var body: some View {
-        Text("Badge")
+        Path { path in
+            let width: CGFloat = 100.0
+            let height = width
+            path.move(to: CGPoint(x: width * 0.95, y: height * 0.20))
+            
+            HexagonParameters.points.forEach {
+                path.addLine(
+                    to: .init(
+                        x: width * $0.useWidth.0 * $0.xFactors.0,
+                        y: height * $0.useHeight.0 * $0.yFactors.0
+                    )
+                )
+            }
+        }
+        .fill(Color.black)
     }
 }
 
